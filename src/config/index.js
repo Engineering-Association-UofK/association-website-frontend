1ddae6a4d3c17d04a6976d6ed1419d9c15fadb7c
Abdulrahim <unvacc80@gmail.com>
2026-04-28 02:00:26 +0300

    update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-export const CONFIG = {
-    API_BASE_URL: (import.meta.env.VITE_API_BASE_URL_RAW || 'http://localhost:8080')+'/api',
-    API_NEW_BASE_URL: (import.meta.env.VITE_API_NEW_BASE_URL_RAW || 'http://localhost:8080')+'/api',
-    API_BASE_URL_RAW: import.meta.env.VITE_API_BASE_URL_RAW || 'http://localhost:8080',
+export const CONFIG = { // DON'T CHANGE THE FALLBACK URL
+    API_BASE_URL: (import.meta.env.VITE_API_BASE_URL_RAW || 'http://localhost:8000')+'/api',
+    API_NEW_BASE_URL: (import.meta.env.VITE_API_NEW_BASE_URL_RAW || 'http://localhost:8000')+'/api',
+    API_BASE_URL_RAW: import.meta.env.VITE_API_BASE_URL_RAW || 'http://localhost:8000',
     API_MONITOR_URL: import.meta.env.VITE_API_MONITOR_URL || 'http://localhost:8888',
     // ADMIN_ROLES: ["sys:admin", "sys:super_admin", "sys:user_manager", "sys:tech_support", "content:editor", "content:blog_manager", "content:event_manager", "content:form_manager", "cert:certifier", "cert:manager", "cert:viewer"],
   TIMEOUT: 10000,
